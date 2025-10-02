[FIX] switch to raw units in StreamDecoder

apps/esp_logger/main.py
@@ -797,6 +797,7 @@ class MonitoringView(QWidget):
         ("mov", "Movesense id"),
         ("mov_bat", "Movesense battery"),
     ]
+    PLOT_DURATION = 10
 
     def __init__(self):
         super().__init__()
@@ -823,13 +824,13 @@ class MonitoringView(QWidget):
         # Create axes with fixed ranges
         self.axis_x = QValueAxis()
         self.axis_x.setTitleText("Time (seconds)")
-        self.axis_x.setRange(0, 9)  # For 10 data points (0-9)
+        self.axis_x.setRange(-self.PLOT_DURATION, 0)
         self.axis_x.setGridLineVisible(False)  # Hide X-axis grid lines
         self.axis_x.setVisible(False)
 
         self.axis_y = QValueAxis()
         self.axis_y.setTitleText("ECG (V)")
-        self.axis_y.setRange(0, 10)  # Match your random data range
+        self.axis_y.setRange(-1, 1)
         self.axis_y.setGridLineVisible(False)  # Hide Y-axis grid lines
 
         # Add axes to chart
@@ -1240,13 +1241,12 @@ class MainWindow(QWidget):
             return
 
         t = time.time()
-        x_time = np.asarray(self.backend.device.plot_x) - t
-        y_ecg = np.asarray(self.backend.device.plot_y)
+        x_time = np.asarray(self.backend.device.plot_x) / 1000 - t
+        y_ecg = np.asarray(self.backend.device.plot_y) * 0.38147e-6
         self.monitoring_view.series.replaceNp(x_time.astype(float), y_ecg.astype(float))
 
         maxY = np.max(np.abs(y_ecg))
         self.monitoring_view.axis_y.setRange(-maxY, maxY)
-        self.monitoring_view.axis_x.setRange(-10, 0)
 
     def reset_graph(self):
         self.monitoring_view.series.clear()

apps/multi_movesense/main.py
@@ -940,7 +940,7 @@ class MovesenseChart(QWidget):
         chart.addSeries(self.series_ecg)
         chart.setTitle(movesense_id)
         axis_x = QValueAxis()
-        axis_x.setRange(-self.PLOT_DURATION, 0)
+        axis_x.setRange(-self.PLOT_DURATION * 1000, 0)
         axis_x.setVisible(False)
         self.axis_ecg = QValueAxis()
         self.axis_ecg.setRange(-1, 1)
@@ -967,7 +967,7 @@ class MovesenseChart(QWidget):
         chart = QChart()
         chart.addSeries(self.series_acc)
         axis_x = QValueAxis()
-        axis_x.setRange(-self.PLOT_DURATION, 0)
+        axis_x.setRange(-self.PLOT_DURATION * 1000, 0)
         axis_x.setVisible(False)
         self.axis_acc = QValueAxis()
         self.axis_acc.setRange(-1, 1)  # Match your random data range
@@ -1453,7 +1453,7 @@ class MainWindow(QWidget):
         if self.current_state != GUIState.MONITORING:
             return
 
-        t = time.time()
+        t = time.time() * 1000
 
         for ms_id, chart in self.monitoring_view.charts.items():
             ecg_data = np.asarray(self.backend.ecg_data[ms_id])
@@ -1647,20 +1647,21 @@ class Backend:
         timestamps, samples, path = data
         if timestamps is not None and samples is not None:
             if device.movesense_id not in self.time_origins:
-                self.time_origins[device.movesense_id] = time.time() - timestamps[0]
+                self.time_origins[device.movesense_id] = (
+                    time.time() * 1000 - timestamps[0]
+                )
 
             origin = self.time_origins[device.movesense_id]
 
             sensor = path.split("/")[2]
-
             if self._logging:
                 if sensor == "ECG":
                     s_array = np.asarray(samples)
-                    s_array = np.round(s_array / 0.38147e-6)
+                    s_array = np.round(s_array)
                     s_array = s_array.astype(int).tolist()
                     ecg_sample = {
                         "ecg": {
-                            "Timestamp": int(timestamps[0] * 1000),
+                            "Timestamp": int(timestamps[0]),
                             "Samples": s_array,
                         }
                     }
@@ -1669,13 +1670,13 @@ class Backend:
                 elif sensor == "IMU6":
                     gyro_sample = {
                         "gyroscope": {
-                            "Timestamp": int(timestamps[0] * 1000),
+                            "Timestamp": int(timestamps[0]),
                             "ArrayGyro": [],
                         }
                     }
                     acc_sample = {
                         "acc": {
-                            "Timestamp": int(timestamps[0] * 1000),
+                            "Timestamp": int(timestamps[0]),
                             "ArrayAcc": [],
                         }
                     }