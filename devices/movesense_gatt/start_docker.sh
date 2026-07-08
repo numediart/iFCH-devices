fd0ca531ab654c4e9f7dc226a7e0c4cc3a6900cf
M. Francois <10929150+MarelliF@users.noreply.github.com>
2026-07-08 16:57:28 +0200

    FIX: update docker mount flags for SEL

--- devices/movesense_gatt/start_docker.sh
+++ devices/movesense_gatt/start_docker.sh
@@ -3,5 +3,5 @@
 # Start an interactive Movesense build container mounted to this project directory.
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker run -it --rm -w /movesense -v $SCRIPT_DIR:/movesense:delegated movesense/sensor-build-env:2.2
+docker run -it --rm -w /movesense -v $SCRIPT_DIR:/movesense:Z movesense/sensor-build-env:2.2
 
